13f035d63dd6df22309e22002fc11f86243146ee
Jacob Porciuncula <jacobporci@gmail.com>
2021-01-20 20:53:50 +0800

    Play with nested routes

--- src/App.js
+++ src/App.js
@@ -6,15 +6,31 @@ import {
 import "./App.css";
 import Layout from "./Layout/Layout";
 
-const InnerRoute = () => {
+const TertiaryRoute = () => {
   const { path } = useRouteMatch();
   return (
     <>
-      <Route path={`${path}/test2`}>
-        <h1>test2</h1>
+      <Route exact path={path}>
+        <h1>exact</h1>
       </Route>
-      <Route path={`${path}/test3`}>
-        <h1>test3</h1>
+      <Route exact path={`${path}/new`}>
+        <h1>/new</h1>
+      </Route>
+    </>
+  );
+};
+
+const InnerRoute = () => {
+  const { path } = useRouteMatch();
+  console.log({ path });
+  return (
+    <>
+      <Route path={`${path}contacts`}>
+        <h1>/contacts</h1>
+      </Route>
+      <Route path={`${path}list`}>
+        <h1>/list</h1>
+        <TertiaryRoute />
       </Route>
     </>
   );
@@ -25,16 +41,16 @@ const App = () => {
     <Router>
       <Route>
         <Layout>
-          <Route exact path="/">
+          <Route path="/">
             <h1>/</h1>
             <InnerRoute />
           </Route>
           <Route path="/a">
-            <h1>test</h1>
+            <h1>/a</h1>
             <InnerRoute />
           </Route>
           <Route path="/b">
-            <h1>test</h1>
+            <h1>/b</h1>
             <InnerRoute />
           </Route>
         </Layout>
